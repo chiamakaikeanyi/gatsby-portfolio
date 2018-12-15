64a4273d975dad78b608c8dbec41faaf930790a0
Chiamaka Ikeanyi <chiamakaikeanyi@gmail.com>
2018-12-15 18:51:29 +0100

    Update config

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ const config = {
   siteURL: `http://chiamakaikeanyi.netlify.com`,
   tagline: 'Chiamaka Ikeanyi | Software Engineer',
   profilePic: profilePic,
-  description: 'Chiamaka Ikeanyi is a Software Engineer based in Lagos, Nigeria. She builds solutions with great user experience using best practices. She is a Front-end Engineer at Konga Online Shopping where she collaborate with awesome engineers to build amazing products for a huge customer base. In addition to her development work, Chiamaka is a technical writer and editor at Facebook Developer Circles Lagos and freeCodeCamp. She also writes musings on Poesie',
+  description: 'Chiamaka Ikeanyi is a Software Engineer based in Lagos, Nigeria. She builds solutions with great user experience using best practices. She is a Front-end Engineer at Konga Online Shopping where she collaborate with awesome engineers to build amazing products for a huge customer base. In addition to her development work, Chiamaka is a technical writer at Facebook Developer Circles Lagos and freeCodeCamp. She also writes musings on Poesie',
   keywords: 'chiamaka ikeanyi, chiamakaikeanyi, software engineer, software engineering, front-end engineer, discurva, women in tech, code, programming, women, tech, javascript, html, css, technical writer',
   themeColor: '#4DB39A',
   twitterCardType: 'summary_large_image',
@@ -19,7 +19,6 @@ const config = {
   stackoverflow: 'https://stackoverflow.com/users/7000929/chiamaka-ikeanyi',
   twitter: 'https://twitter.com/chiamakaikeanyi',
   medium: 'https://medium.com/@chiamakaikeanyi',
-  shareImage: 'logo',
   imageWidth: 1200,
   imageHeight: 1200,
 };
